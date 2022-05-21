fd27a9fa904a9b40ee1fe608dd3624d641eead49
Ryan Fetterman <rff5006@gmail.com>
2022-05-20 21:50:22 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "784432008842616831"; // your league ID
+export const leagueID = "784432008842616832"; // your league ID
 export const leagueName = "Dynasty Nubcakes"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
